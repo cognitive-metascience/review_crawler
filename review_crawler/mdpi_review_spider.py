756bbb9d9b2dec81396d0a0f1819eb26db7f937d
x-j <xjasienski@gmail.com>
2022-06-23 00:36:12 +0200

    split data into reviews and responses

--- review_crawler/mdpi_review_spider.py
+++ review_crawler/mdpi_review_spider.py
@@ -9,8 +9,9 @@ import os
 HARD_SPACE_REGEX = re.compile(r'\xa0')  # cleans up no-break spaces
 REPEATING_REVIEWS= "This manuscript is a resubmission of an earlier submission. The following is a list of the peer review reports and author responses from that submission."
 NUMBERS_PATTERN = re.compile(r"\d+")
-ROUND_NUMBER_PATTERN = re.compile(r"Round \d+")
-REVIEWER_REPPORT_PATTERN = re.compile(r"Reviewer \d+ Report")
+ROUND_NUMBER_PATTERN = re.compile(r"^Round \d+$")
+REVIEWER_REPORT_PATTERN = re.compile(r"^Reviewer \d+ Report$")
+AUTHOR_RESPONSE_PATTERN = re.compile(r"^Author Response$")
 DOI_PATTERN = re.compile(r"https://doi\.org/10.\d{4,9}/[-._;()/:a-zA-Z0-9]+")  # from https://www.crossref.org/blog/dois-and-matching-regular-expressions/
 
 class MdpiReviewSpider(scrapy.Spider):
@@ -47,11 +48,13 @@ class MdpiReviewSpider(scrapy.Spider):
             if not os.path.exists(os.path.join(self.dump_dir, dir, 'metadata.json')):
                 self.logger.warning(f"Unexpectedly, didn't find file with metadata in dump_dir/{dir}")
                 continue
-            
             with open(os.path.join(self.dump_dir, dir, 'metadata.json'), 'r') as fp:
-                meta = json.load(fp)
-                if meta['has_reviews']:
-                    urls.append(meta['reviews_url']) 
+                try:
+                    meta = json.load(fp)
+                    if meta['has_reviews']:
+                        urls.append(meta['reviews_url']) 
+                except json.JSONDecodeError:
+                    self.logger.error("Error while reading metadata from "+dir)
         self.logger.info(f'Found {len(urls)} urls in dump_dir with reviews to scrape.')
         return urls
 
@@ -69,8 +72,6 @@ class MdpiReviewSpider(scrapy.Spider):
             self.log('Parsing reviews from subpage: /review_report')
             yield from self.parse_reviews(response)
 
-    # Remember: D6
-
     def parse_reviews(self, response):
         div = response.css('div.bib-identity')
         original_a_doi = DOI_PATTERN.search(div.get()).group()
@@ -80,80 +81,128 @@ class MdpiReviewSpider(scrapy.Spider):
             sub_a_dir =  a_short_doi + '/sub-articles'
             os.makedirs(os.path.join(self.dump_dir, sub_a_dir), exist_ok=True)
         
-        reviewers = []
+        # find reviewers' names
+        reviewers = {}
         for div in response.css('div[style="display: block;font-size:14px; line-height:30px;"]'):
             texts = [x.get().strip() for x in div.css('::text')]
-            reviewers.append({
-                'number': re.search(NUMBERS_PATTERN, texts[0]).group(),
-                'name': texts[1].strip()})
-        i = 1
+            r_no = int(re.search(NUMBERS_PATTERN, texts[0]).group())
+            reviewers[r_no] = {'number': r_no, 'name': texts[1].strip()}
+        
+        i = 0
+        rev_no = 0
         ard = {}
         dump  = False
-        for p in response.css('div.abstract_div p, ul'):
+        clean_prev_ard = False
+
+        # find the div with article content and iterate over it
+        absdiv = response.xpath('.//div[@class="abstract_div"]/div[not(@*)]')
+        for p in absdiv.xpath('./p | .//li'):
             soup = BeautifulSoup(p.get(), 'lxml')
-            text = soup.get_text().strip()
+            text = HARD_SPACE_REGEX.sub('', soup.get_text().strip())
             if REPEATING_REVIEWS in text:
                 break
-            for span in p.css('span[style="font-size: 18px; margin-top:10px;"]'):
-                span_text = HARD_SPACE_REGEX.sub('', span.css('::text').get())  #
-                if ROUND_NUMBER_PATTERN.match(span_text):
-                    round_no = NUMBERS_PATTERN.search(span_text).group()
-                    ard = {
-                            'url': response.url,
-                            'original_article_doi': original_a_doi,
-                            'original_article_url': response.url[:response.url.rfind('review')],
-                            'type': "aggregated-review-documents",
-                            'reviewers': reviewers,
-                            'round': round_no,
-                            'supplementary_materials': []
-                        }
-                    for a in response.css('div#abstract.abstract_div div p a'):
-                        file_url = a.css('::attr(href)').get()
-                        orig_filename = a.css('::text').get().strip()
-                        filex = os.path.splitext(orig_filename)[1]
-                        # arbitrary generaion of id's
-                        sm_id = a_short_doi+'.s'+str(i)
-                        i+=1
-                        try:
-                            sm_type = file_url.split('=')[1][:file_url.split('=')[1].rfind('&')]
-                        except:
-                            sm_type = 'NA'
-                        ard['supplementary_materials'].append({
-                            'id': sm_id, 'filename': sm_id+filex, 'type' :sm_type,
-                            'original_filename': orig_filename, 'url': file_url,
-                            'title': os.path.splitext(orig_filename)[0] 
-                        })
-
-                    if self.dump_dir is not None:
-                        # download supplementary materials:
-                        for sm in ard['supplementary_materials']:
-                            sm_path = os.path.join(self.dump_dir, sub_a_dir, sm['filename'])
-                            if os.path.exists(sm_path) and not self.update:
-                                self.logger.info(f"{sm['filename']} already exists. Will NOT overwrite.")
-                            else:
-                                with open(sm_path, 'wb') as f:
-                                    self.logger.debug(f"Downloading supplementary material from {sm['url']}")
-                                    r = requests.get(sm['url'], stream=True)
-                                    f.write(r.content)
-                                    self.files_dumped_counter += 1
-                        # save plaintext to files
-                        filen = a_short_doi+'.r'+round_no
-                        filename = filen +'.txt'
-                        ard['supplementary_materials'].append(
-                            {'filename':filename,  'id':filen, 
-                            'title':"This sub_article in plaintext."})
-                        self.dump_metadata(ard, sub_a_dir, filen)
-                        if dump:
-                            fp.close()
-                        # open a file for writing plaintext article content
-                        fp = open(os.path.join(self.dump_dir, sub_a_dir, filename), 'w+')
+            # look for a "Round #" heading 
+            if ROUND_NUMBER_PATTERN.match(text):
+                round_no = int(NUMBERS_PATTERN.search(text).group())
+                i=1
+                continue
+            # look for the start of a review
+            elif REVIEWER_REPORT_PATTERN.match(text):
+                if len(ard) > 0:
+                    clean_prev_ard = True
+                    prev_ard = ard
+                if dump:
+                    fp.close()
+                    self.files_dumped_counter += 1
+                rev_no = int(NUMBERS_PATTERN.search(text).group())
+                ard = {
+                        'url': response.url,
+                        'id' : f"{a_short_doi}.r{round_no}{rev_no}",
+                        'original_article_doi': original_a_doi,
+                        'original_article_url': response.url[:response.url.rfind('review')],
+                        'type': "review",
+                        'reviewer': reviewers[rev_no],
+                        'round': round_no,
+                        'supplementary_materials': []
+                    }
+                if self.dump_dir is not None:
+                    filen = ard['id']
+                    filename = filen +'.txt'
+                    ard['supplementary_materials'].append(
+                        {'filename':filename,  'id':filen, 
+                        'title':"This sub_article in plaintext."})
+                    # open a file for writing plaintext article content
+                    fp = open(os.path.join(self.dump_dir, sub_a_dir, filename), 'w+')
+                    dump = True
+                
+            # look for the start of an author response
+            elif AUTHOR_RESPONSE_PATTERN.match(text):
+                if len(ard) > 0:
+                    clean_prev_ard = True
+                    prev_ard = ard
+                    if dump:
+                        fp.close()
                         self.files_dumped_counter += 1
-                        dump = True
-                        
-                    yield ard
+                ard = {
+                    'url': response.url,
+                    'id' : f"{a_short_doi}.a{round_no}{rev_no}",
+                    'original_article_doi': original_a_doi,
+                    'original_article_url': response.url[:response.url.rfind('review')],
+                    'type': "author-comment",
+                    'round': round_no,
+                    'replying_to': rev_no,
+                    'supplementary_materials': []
+                }
+                if self.dump_dir is not None:
+                    filen = ard['id']
+                    filename = filen +'.txt'
+                    ard['supplementary_materials'].append(
+                        {'filename':filename,  'id':filen, 
+                        'title':"This sub_article in plaintext."})
+                    # open a file for writing plaintext article content
+                    fp = open(os.path.join(self.dump_dir, sub_a_dir, filename), 'w+')
+                    dump = True
+            
+            # look for links to supplementary materials
+            for a in p.xpath('.//a'):
+                file_url = a.css('::attr(href)').get()
+                orig_filename = a.css('::text').get().strip()
+                filex = os.path.splitext(orig_filename)[1]
+                # arbitrary generaion of id's
+                sm_id = f"{a_short_doi}.s{ard['round']}{i}"
+                i+=1
+                try:
+                    sm_type = file_url.split('=')[1][:file_url.split('=')[1].rfind('&')]
+                except:
+                    sm_type = 'NA'
+                sm = {
+                    'id': sm_id, 'filename': sm_id+filex, 'type' :sm_type,
+                    'original_filename': orig_filename, 'url': file_url,
+                    'title': os.path.splitext(orig_filename)[0] 
+                }
+                ard['supplementary_materials'].append(sm)
+                # download this supplementary material:
+                if self.dump_dir is not None:
+                    sm_path = os.path.join(self.dump_dir, sub_a_dir, sm['filename'])
+                    if os.path.exists(sm_path) and not self.update:
+                        self.logger.info(f"{sm['filename']} already exists. Will NOT overwrite.")
+                    else:
+                        with open(sm_path, 'wb') as f:
+                            self.logger.debug(f"Downloading supplementary material from {sm['url']}")
+                            r = requests.get(sm['url'], stream=True)
+                            f.write(r.content)
+                            self.files_dumped_counter += 1
+            if clean_prev_ard:
+                yield prev_ard
+                if dump:
+                    self.dump_metadata(prev_ard, sub_a_dir, prev_ard['id'])
+                clean_prev_ard = False
             if dump:
                 fp.write(text+'\n')
-        
+        yield ard
+        if dump:
+            self.dump_metadata(ard, sub_a_dir, ard['id'])
+    
         if self.dump_dir is not None:
             self.logger.info(f"Dumped {self.files_dumped_counter} files so far.")
                     
@@ -165,11 +214,14 @@ class MdpiReviewSpider(scrapy.Spider):
         div = response.css('div.bib-identity')
         original_a_doi = DOI_PATTERN.search(div.get()).group()
         a_short_doi = self.shorten_doi(original_a_doi)
+        ard_id = a_short_doi+'.r1'
+
         if self.dump_dir is not None:
             sub_a_dir =  a_short_doi + '/sub-articles'
             os.makedirs(os.path.join(self.dump_dir, sub_a_dir), exist_ok=True)
         ard = {
                 'url': response.url + '#review_report',
+                'id': ard_id,
                 'original_article_doi': original_a_doi,
                 'original_article_url': response.url,
                 'type': "aggregated-review-documents",
@@ -201,7 +253,7 @@ class MdpiReviewSpider(scrapy.Spider):
                             fp.write(r.content)
                             self.files_dumped_counter += 1
         if self.dump_dir is not None:
-            self.dump_metadata(ard, sub_a_dir, a_short_doi+'.r')    # todo: change the naming convention?
+            self.dump_metadata(ard, sub_a_dir, ard_id)    # todo: change the naming convention?
         yield ard
                 
     def dump_metadata(self, metadata, dirname=None, filename='metadata'):
@@ -225,7 +277,7 @@ class MdpiReviewSpider(scrapy.Spider):
                 self.logger.info(f"metadata already exists in {dirname}. Will NOT overwrite.")
             else:
                 if os.path.exists(filepath):
-                    self.logger.warning(f"metadata already exists in {dirname}. Will overwrite!")
+                    self.logger.info(f"metadata already exists in {dirname}. Will overwrite!")
                 with open(filepath, 'w+', encoding="utf-8") as fp:
                     json.dump(metadata, fp, ensure_ascii=False)
 
